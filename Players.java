c12e3cef8290fd72c1ab63496d2852204507576e
nourshinnawi <50388570+nourshinnawi@users.noreply.github.com>
2019-06-04 13:27:08 -0700

    Update Players.java

--- Players.java
+++ Players.java
@@ -1,46 +1,19 @@
 package nba;
+import java.util.List;
 import java.util.ArrayList;
-import java.util.Comparator;
 
-public class Players implements Comparable<Players> {
+public class Teams {
 
-    private Teams team;
-    private int jerseyNumber;
-    private String position;
     private String name;
+    private int identifier;
+    private final String record;
     
-    public Players(Teams team, int jerseyNumber, String position, String name)
-    {
-        this.team = team;
-        this.jerseyNumber = jerseyNumber;
-        this.position = position;
+    public Teams(String name, Integer identifier, String record)   {
         this.name = name;
+        this.identifier = identifier;
+        this.record = record;
     }
 
-    public Teams getTeam() {
-        return team;
-    }
-
-    public void setTeam(Teams team) {
-        this.team = team;
-    }
-
-    public int getJerseyNumber() {
-        return jerseyNumber;
-    }
-
-    public void setJerseyNumber(int jerseyNumber) {
-        this.jerseyNumber = jerseyNumber;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-    
     public String getName() {
         return name;
     }
@@ -49,15 +22,28 @@ public class Players implements Comparable<Players> {
         this.name = name;
     }
 
-   @Override
-   public int compareTo(Players o)
-   {
-       return this.name.compareTo(o.name);
-   }   
-   
-   @Override
-   public String toString()
-   {
-       return name + " - " + jerseyNumber + " - " + position;
-   }
+    public Integer getIdentifier() {
+        return identifier;
+    }
+    
+    public void setIdentifier(Integer identifier) {
+        this.identifier = identifier;
+    }
+    
+    public List<Players> players = new ArrayList<>(8);
+    private Coaches coach;
+
+    public Coaches getCoach() {
+        return coach;
+    }
+
+    public void setCoach(Coaches coach) {
+        this.coach = coach;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return  "  : " + record + "\n";
+    }
 }
